f536fe9524c61b5a2581cb9ce4602ffc97ffc241
pengyuyan <1023092893@qq.com>
2023-09-27 00:42:55 +0800

    for first

---
+++ cal_time.py
@@ -0,0 +1,12 @@
+import time
+
+
+def cal_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"{func.__name__} 执行时间: {execution_time} 秒")
+        return result
+    return wrapper
